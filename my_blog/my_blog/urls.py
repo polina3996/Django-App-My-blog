91bd478d1c0b140b867720dbbb488a98b4b0c07d
PalinaK <polina.kirichencko@yandex.ru>
2024-01-24 15:03:24 +0100

    new models in Stories app(Stories, Category and Tag)

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
+from my_blog.my_blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# when DEBUG is True, we have one more URL (to downloaded files) for test server to take these
+# files on MEDIA_URL and view on HTML page
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_blog/stories/models.py
+++ my_blog/stories/models.py
@@ -1,3 +1,97 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    """Manager that takes from the database only PUBLISHED stories"""
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Stories.Status.PUBLISHED)
+
+
+class Stories(models.Model):
+    class Status(models.IntegerChoices):
+        """Class with options for the story (published or not)"""
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug", validators=[
+        MinLengthValidator(5, message="Минимум 5 символов"),
+        MaxLengthValidator(100, message="Максимум 100 символов"),
+    ])
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", default=None,
+                              blank=True, null=True, verbose_name="Фото")
+    content = models.TextField(blank=True, verbose_name="Текст рассказа")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="Статус")
+
+    # object of model Category.
+    # attribute + field in Stories: 'cat_id'
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='stories', verbose_name="Категории")
+
+    # object of model Tag.
+    # NO field in Stories: new mid-table + attribute
+    tags = models.ManyToManyField('Tag', blank=True, related_name='tags', verbose_name="Теги")
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, related_name='stories', null=True,
+                               default=None)
+
+    # to still exist (stories = Stories.objects.all())
+    objects = models.Manager()
+    # creating a manager, that takes only published stories
+    # instead of 'objects' attribute (stories = Stories.published.all())
+    published = PublishedManager()
+
+    # how the object of the Stories will be displayed
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        """Metadata of the model Stories(in admin-panel)"""
+        verbose_name = "Рассказ"
+        verbose_name_plural = "Рассказы"
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
+
+    def get_absolute_url(self):
+        """Object of the Stories can return URL"""
+        return reverse('story', kwargs={'story_slug': self.slug})
+
+
+class Category(models.Model):
+    """Model with ManyToOne with Stories"""
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    # how the object of the Category will be displayed
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        """Object of the Category can return URL"""
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+
+class Tag(models.Model):
+    """Model with ManyToMany to Stories"""
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    # how the object of the Tag will be displayed
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        """Object of the Tag can return URL"""
+        return reverse('tag', kwargs={'tag_slug': self.slug})
